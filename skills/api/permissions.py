962361167a18bc5542849d98dce3847db3116b4a
Nana Adjedu <nanaadjedu@Nanas-MBP-2.attlocal.net>
2021-09-19 22:06:31 -0400

    modified skills table and related mvc

--- skills/api/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-
-class CanActionUser(permissions.BasePermission):
-    message = "User does not have permission to retrieve User skills."
-
-    def has_object_permission(self, request, view, obj):
-        return obj == request.user
